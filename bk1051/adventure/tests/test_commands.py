967a8aed868b62778f0d5b0412aad31b05bb9a80
Brian Karfunkel <bk1051@nyu.edu>
2015-10-11 21:11:28 -0400

    Added yes/no test to test_commands

--- bk1051/adventure/tests/test_commands.py
+++ bk1051/adventure/tests/test_commands.py
@@ -26,3 +26,37 @@ class CommandTest(TestCase):
             game.do_command(['no'])  # WOULD YOU LIKE INSTRUCTIONS?
             game.do_command(['enter'])  # so we are next to lamp
             game.do_command([word, 'lamp'])
+    
+    def test_only_allow_valid_responses_to_yesno_questions(self):
+        game = Game()
+        # Test valid yes/no responses
+        valid_yes_responses = ['y', 'yes']
+        valid_no_responses = ['n', 'no']
+        
+        # Test each yes response
+        for response in valid_yes_responses:
+            # Yes responses should correspond to true
+            game.yesno_callback = self.assertTrue
+            game.do_command([response])
+        
+        # Test each no response
+        for response in valid_no_responses:
+            # No responses should correspond to false
+            game.yesno_callback = self.assertFalse
+            game.do_command([response])
+        
+        # Test invalid yes/no responses
+        invalid_responses = ['ye', 'yeah', 'Y', 'Yes', 'No', 'nope', 'nah']
+        # Create a callback that always raises an exception
+        # That way, if do_command recognizes the response as valid,
+        # and thus calls the callback, the test will fail
+        def raise_exception_callback(yes):
+            raise ValueError
+        
+        for response in invalid_responses:
+            game.yesno_callback = raise_exception_callback
+            try:
+                game.do_command([response])
+            except ValueError:
+                raise ValueError("'{}' should not be a valid yes/no response".format(response))
+        
