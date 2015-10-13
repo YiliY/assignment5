c7419e91d883b3fa485eb58f3f71c3b108051ca6
Urjit Patel <up276@nyu.edu>
2015-10-13 00:02:00 -0400

    commit after placing back test_commands in the tests directory

--- up276/adventure/tests/test_commands.py
+++ up276/adventure/tests/test_commands.py
@@ -26,3 +26,17 @@ class CommandTest(TestCase):
             game.do_command(['no'])  # WOULD YOU LIKE INSTRUCTIONS?
             game.do_command(['enter'])  # so we are next to lamp
             game.do_command([word, 'lamp'])
+            
+    def test_do_command_method_for_yesno_answers(self):
+        
+            game = Game()                          # Instance creation for the Game() class   
+            load_advent_dat(game)
+            game.start()
+            game.yesno_callback = self.assertTrue  # This will test if argument to callback is True
+            game.do_command(['y'])                 # 'y' is passed to 'do_command' method - To test the 'y' case 
+            game.yesno_callback = self.assertTrue  # This will test if argument to callback is True
+            game.do_command(['yes'])               # 'yes' is passed to 'do_command' method - To test the 'yes' case
+            game.yesno_callback = self.assertFalse # This will test if argument to callback is False
+            game.do_command(['n'])                 # 'n' is passed to 'do_command' method - To test the 'n' case
+            game.yesno_callback = self.assertFalse # This will test if argument to callback is False
+            game.do_command(['no'])                # 'no' is passed to 'do_command' method - To test the 'no' case
